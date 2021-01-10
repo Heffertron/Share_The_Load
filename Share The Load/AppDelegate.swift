1d9ac4ab59504b00c65271ec233398e3ac29d741
Chris Hefferman <chrishefferman@me.com>
2021-01-10 22:15:43 +0000

    Added Firebase configuration

--- Share The Load/AppDelegate.swift
+++ Share The Load/AppDelegate.swift
@@ -6,14 +6,14 @@
 //
 
 import UIKit
+import Firebase
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
